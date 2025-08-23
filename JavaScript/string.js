06476966419fab61ab5904f92dca7471495bd31f
pranshulsharma13 <pranshulsharmaalg@gmail.com>
2025-08-23 14:53:46 +0530

    string

---
+++ JavaScript/string.js
@@ -0,0 +1,22 @@
+const str = "hello world";
+const str1 = 'pranshul';
+console.log(str.length);
+console.log(str.indexOf("o"));
+console.log(str.lastIndexOf("o"));
+console.log(str1.slice(0,5));
+console.log(str1.replace("pran" , "rit"));
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+console.log(str1.concat(" sharma"));
+console.log(str1.charAt(3));
+console.log(str1.charCodeAt(3));
+
+//SPLIT
+console.log(str1.split(""));
+console.log(str1.split("s"));
+console.log(str1.split("r"));
+console.log(str1.split(" ",3));
+
+console.log(str.search("o"));
+let result = str1.match("pranshul");
+console.log(result);
