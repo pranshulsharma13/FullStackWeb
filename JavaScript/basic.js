694f35fc810d60320d2c411e04c55356335557ba
pranshulsharma13 <pranshulsharmaalg@gmail.com>
2025-08-23 11:37:56 +0530

    basic

---
+++ JavaScript/basic.js
@@ -0,0 +1,35 @@
+// var name = "pranshul";
+// if(name == "pranshul"){
+//     console.log("true");
+// }
+// else{
+//     console.log("False");
+// }
+
+// var name = "ritik";
+// console.log(name);
+
+
+// if(true){
+//     let name = "pranshul";
+//     console.log(name);
+// }
+// // let name = "ritik";
+// console.log(name);
+
+
+
+// if(true){
+//     const name = "pranshul";
+//     console.log(name);
+// }
+// const name = "ritik";
+// console.log(name);
+
+
+let firstname ="pranshul";
+let lastname = "sharma";
+let fullname = firstname +     lastname;
+console.log(fullname);
+let fullname2 = `${firstname}    ${lastname}`;
+console.log(fullname2);
---
+++ JavaScript/function.js
@@ -0,0 +1,71 @@
+// //function parameters and arguments
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(2,5)); //7
+
+// //function expression
+// var result = function sum(a,b){
+//     console.log(a+b);
+//     return a+b;
+// };
+// result(2,5); 
+
+// // Anonymous function
+// var result = function(a,b){
+//     console.log(a+b);
+//     return a+b;
+// };
+
+// result(2,5);
+
+// console.log(result(2,5)); //without return it is undefined
+
+// // Immediately Invoked Function Expression (IIFE)
+// (function(a,b){
+//     console.log(a+b);
+// })
+
+// (5,4);
+
+// Arrow function
+// var sum = (a,b) => {
+//     return a+b;
+// }
+// console.log(sum(5,4));
+
+
+
+// var sum = (a,b) => a+b; 
+// console.log(sum(5,4));
+
+
+// //square of a number in one line
+// var square = a => a*a;
+// console.log(square(5));
+
+
+//calculator
+// var cal = (a ,b , operator) =>{
+//     var result;
+//     switch(operator){
+//         case '+': result =a+b; return result;
+//         case '-': result =a-b; return result;
+//         case '*': result =a*b; return result;
+//         case '/': result =a/b; return result;
+//     }
+// }
+// console.log(cal(5,4,'*'));
+
+//Reverse
+// const isreverse = (str) => {
+//     let rev = "";
+//     for(let char = str.length-1; char >= 0 ;char--){
+//         rev = rev + str[char];
+        
+//     }
+//     return rev;
+// }
+// console.log(isreverse("pranshul"));
+
+
