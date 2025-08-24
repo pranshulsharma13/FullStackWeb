35a3a117dddd78e9685342e643821d6249ec8710
pranshulsharma13 <pranshulsharmaalg@gmail.com>
2025-08-24 22:41:33 +0530

    string

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -1,22 +1,27 @@
-const str = "hello world";
-const str1 = 'pranshul';
-console.log(str.length);
-console.log(str.indexOf("o"));
-console.log(str.lastIndexOf("o"));
-console.log(str1.slice(0,5));
-console.log(str1.replace("pran" , "rit"));
-console.log(str1.toUpperCase());
-console.log(str1.toLowerCase());
-console.log(str1.concat(" sharma"));
-console.log(str1.charAt(3));
-console.log(str1.charCodeAt(3));
+// const str = "hello world";
+// const str1 = 'pranshul';
+// console.log(str.length);
+// console.log(str.indexOf("o"));
+// console.log(str.lastIndexOf("o"));
+// console.log(str1.slice(0,5));
+// console.log(str1.replace("pran" , "rit"));
+// console.log(str1.toUpperCase());
+// console.log(str1.toLowerCase());
+// console.log(str1.concat(" sharma"));
+// console.log(str1.charAt(3));
+// console.log(str1.charCodeAt(3));
 
-//SPLIT
-console.log(str1.split(""));
-console.log(str1.split("s"));
-console.log(str1.split("r"));
-console.log(str1.split(" ",3));
+// //SPLIT
+// console.log(str1.split(""));
+// console.log(str1.split("s"));
+// console.log(str1.split("r"));
+// console.log(str1.split(" ",3));
 
-console.log(str.search("o"));
-let result = str1.match("pranshul");
-console.log(result);
+// console.log(str.search("o"));
+// let result = str1.match("pranshul");
+// console.log(result);
+
+
+let text = "hello javascript , welcome to our world"
+let includeResult = text.includes("java");
+console.log(includeResult);
